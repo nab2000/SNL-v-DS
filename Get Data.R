d80a50303de5b40a1041f9b3e4c00e811d4d4d58
nab2000 <nick.dean.rose@gmail.com>
2015-09-01 09:09:42 -0400

    Created get data outline

---
+++ Get Data.R
@@ -0,0 +1,14 @@
+## Getting and Cleaning Data Algorithm
+
+
+## first pull in list of SNL alumni and Daily show alumni
+
+## second pull in movies and tv shows that the actor has appeared in from IMDB
+
+## third pull in Rotten tomatoe review (people not critics) for each movie. 
+
+## prepare this as name of actor, SNL or DS, name of movie/tv show, rating
+
+
+## prepare cleaner data set with actor, mean of ratins, meadian of ratings, sd of ratins, and max 
+## this data set will be used do analysis 
