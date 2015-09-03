dc449b964168d88cec2548fcb00cd8cb74252562
nab2000 <nick.dean.rose@gmail.com>
2015-09-02 23:21:34 -0400

    Changes to pull in list of SNL and Daily Sow Actors

--- Get Data.R
+++ Get Data.R
@@ -1,14 +1,37 @@
 ## Getting and Cleaning Data Algorithm
-library("RCurl")
-library("jsonlite")
-
+library(RCurl)
+library(jsonlite)
+library(XML)
 
 ## first pull in list of SNL alumni and Daily show alumni, wikepedia
 
+SNLurl <- "https://en.wikipedia.org/wiki/Saturday_Night_Live_cast_members"
+tabs_SNL <- getURL(SNLurl)
+tab_SNL <- readHTMLTable(tabs)
+sub_SNL <- tab[[2]]
+snl_actors <- as.character(sub[,1])
 
+DSurl <- "https://en.wikipedia.org/wiki/List_of_The_Daily_Show_correspondents"
+tabs_DS <- getURL(DSurl)
+tab_DS <- readHTMLTable(tabs_DS)
+sub_DS1 <- tab_DS[[2]]
+sub_DS2 <- tab_DS[[3]]
+sub_DS3 <- tab_DS[[4]]
+sub_DS4 <- tab_DS[[5]]
+sub_DS5 <- tab_DS[[6]]
+DS_actors <- c(as.character(sub_DS1[,1]), as.character(sub_DS2[,1]),
+        as.character(sub_DS3[,1]), as.character(sub_DS4[,1]), 
+                                     as.character(sub_DS5[,1]))
+        
+        
 ## second pull in movies and tv shows that the actor has appeared in from IMDB
+## use this first one to call a search then use id of first name to 
+## call the actor (id starts with nm) 
 http://www.imdb.com/xml/find?json=1&nr=1&nm=on&q=jeniffer+garner
-http://www.imdb.com/xml/find?xml=1&nr=1&tt=on&q=lost
+http://www.imdb.com/name/nm0002071/?ref_=fn_al_nm_1
+
+## json call http://www.imdb.com/xml/find?json=1&nr=1&nm=on&q=jeniffer+garner
+##xml call http://www.imdb.com/xml/find?xml=1&nr=1&tt=on&q=lost
 
 ## third pull in Rotten tomatoe review (people not critics) for each movie. 
 
