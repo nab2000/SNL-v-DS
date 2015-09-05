4a5b3333c334db5cdb5d157603c614dcb5005867
Nick Rose <nick.dean.rose@gmail.com>
2015-09-04 21:02:23 -0400

    Update Get Data.R with year columns done

--- Get Data.R
+++ Get Data.R
@@ -10,13 +10,21 @@ tabs_SNL <- getURL(SNLurl)
 tab_SNL <- readHTMLTable(tabs_SNL)
 sub_SNL <- tab_SNL[[2]]
 SNL_actors <- as.character(sub_SNL[,1])
-## modifying year formats
-## need if charcter lenght is longer than 9 (4 years-4years) then pull first 4 characters and last four 
-## then use strsplit to separate into begin and end by -
+## modifying year formats, may need for loop for 
 SNL_actors_years <- as.character(sub_SNL[,2])
+StartYear <- data.frame(NULL)
+LastYear <- data.frame(NULL)
+
+For (i in 1:length(SNL_actors_years) {
+StartYear[i] <- substr(SNL_actors_years[i], 1, 4)
+}
+
+For(i in 1:length(SNL_actors_years) {
+LastYear[i] <- substr(SNL_actors_years[i], nchar(SNL_actors_years[i])-3,  nchar(SNL_actors_years[i]))
+}
 
 df_SNL <- data.frame(Actor = SNL_actors, Show = rep("SNL", length(snl_actors)), 
-                     BegYear = , EndYear = )
+                     BegYear = StartYear , EndYear = LastYear)
 
 
 
@@ -39,30 +47,34 @@ DS_actors_yearleft <- c(rep(2016, length(sub_DS1[,2])), rep(2016, length(sub_DS2
                         rep(2016, length(sub_DS3[,2])), as.character(sub_DS4[,3]), 
                         as.character(sub_DS5[,3]))  
 
-df_DS <- data.frame(Actor = DS_actors, Show = rep("Daily Show", length(DS_actors)), YearJoin = DS_actors_yearjoin, YearLeft = DS_actors_yearleft)
-## removes people yet ot join show
+df_DS <- data.frame(Actor = DS_actors, Show = rep("Daily Show", length(DS_actors)), BegYear = DS_actors_yearjoin, EndYear = DS_actors_yearleft)
+## removes people yet to join show
 df_DS <- df_DS[df_DS$YearJoin != "TBA", ]
 
 ## one just year, need to develop work around (== *,*)
+For (a in 1:nrow(df_DS$YearJoin){
+If (nchar(df_DS$YearJoin[a] != 4) {
 df_DS$YearJoin <- as.Date(df_DS$YearJoin, "%B %d, %Y")
 df_DS$YearJoin <- format(df_DS$YearJoin, "%Y")
+}
  
- 
-## Need to clean up 
+Actor_df <- rbind(df_SNL, df_DS)
+
                           
 ## second pull in movies and tv shows that the actor has appeared in from IMDB
 ## use this first one to call a search then use id of first name to 
 ## call the actor (id starts with nm) 
 
 ## need to create for loop here:
-
-split_name <- strsplit(name, " ")
+for(a in 1: nrow(Actor_df)) {
+split_name <- strsplit(Actor_df$Actor[a], " ")
 first_name <- split_name[[1]][1]
 last_name <- split_name[[1]][2]
 
 imdb <- "http://www.imdb.com/xml/find?json=1&nr=1&nm=on&q="
 actor_searchURL <- paste(imdb, first_name, "+", last_name, sep ="")
 result_search <- getURL(actor_searchURL)
+
 ## need ot figure otu how to pull first id (nm... and remvoe backspace)
 id <- 
 
@@ -72,6 +84,7 @@ result_endURL <- "/?ref_=fn_al_nm_1"
 actor_URL <- paste (result_begURL, id, result_endURL, sep = "")
 actor_page <- getURL(actor_URL)
 
+}
 ## json call http://www.imdb.com/xml/find?json=1&nr=1&nm=on&q=jeniffer+garner
 ##xml call http://www.imdb.com/xml/find?xml=1&nr=1&tt=on&q=lost
 
