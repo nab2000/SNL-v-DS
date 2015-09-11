3a9ec0d3ea18d51d9e069db6b5c8b7ec7c0614a6
nab2000 <nick.dean.rose@gmail.com>
2015-09-11 14:00:22 -0400

    Add category to movie list and some debuggin of RT get

--- Get_RT_Score.R
+++ Get_RT_Score.R
@@ -2,70 +2,93 @@
 add_RTscores <- function(dir = "./", save_name = "movie_list_RT", load_file = "Movie_List.csv", 
                          load_vector = "movie_df"){
 
-require(RCurl)
-require(XML)
-
-
-file_loc <- paste(dir, load_file, sep ="")
-
-if (exists(load_vector)){
-    movie_df <- get(load_vector)
+    require(RCurl)
+    require(XML)
     
-}
-else if (file.exists(file_loc)){
-    movie_df <- read.csv(file_loc, check.names = F)
     
-}
-
-else { print("file does not exist"); stop }
-
-## clean up movie dataframe to remove SNL and daily show movies/tv shows
-movie_df[,1] <- as.character(movie_df[,1])
-movie_df[,2] <- as.character(movie_df[,2])
-sub <- grep("SNL", movie_df[,2])
-movie_df <- movie_df[-sub, ]
-sub <- grep("Saturday Night Live", movie_df[,2])
-movie_df <- movie_df[-sub, ]
-sub <- grep("Daily Show", movie_df[,2])
-movie_df <- movie_df[-sub, ]
-
-movie_score_df <- NULL
-for (i in 1:nrow(movie_df)){
-    rt <- "http://www.rottentomatoes.com/search/?search="
-    search <- gsub(" ", "+", movie_df[i, 2])
-    search <- gsub(":", "", search)
-    movie_searchURL <- paste(rt, search, sep ="")
-    result_search <- getURL(movie_searchURL)
-    result <- htmlTreeParse(result_search)
-    result_root <- xmlRoot(result)
-    result_list <- xmlToList(result_root)
-    result_body <- result_list$body
-    ## this skips any 
-    if (length(result_body1[[14]][[3]][[1]]) == 13) {next}
-    ## this next line pulls in the location of the first movie to match the search criteria
-    title <- result_body[[14]][[3]][[1]][[3]][[3]][[2]][[6]]
+    file_loc <- paste(dir, load_file, sep ="")
     
-    rt <- "http://www.rottentomatoes.com"
-    movie_nameURL <- paste(rt, title[4], sep ="")
-    movie_page <- getURL(movie_nameURL)
-    movie_page_html <- htmlTreeParse(movie_page)
-    movie_root <- xmlRoot(movie_page_html)
-    movie_list <- xmlToList(movie_root)
-    movie_head <- movie_list$head
-    score <- movie-head[[42]][2] 
-    score_num <- as.numeric(substr(score, 1,2))
-    if (is.na(score_num)) {next}
+    if (exists(load_vector)){
+        movie_df <- get(load_vector)
+        
+    }else if (file.exists(file_loc)){
+        movie_df <- read.csv(file_loc, check.names = F)
+        
+    }else { print("file does not exist"); stop }
     
-    movie_score_df[i, 1] <- movie_df[i, 1]
-    movie_score_df[i, 2] <- movie_df[i, 2]
-    movie_score_df[i, 3] <- score_num
-    movie_score_df[i, 4] <- movie_nameURL
-    names(movie_score_df) <- c("Actor", "Movie", "Audience_Score", "URL")
-
+    ## clean up movie dataframe to remove SNL and daily show movies/tv shows
+    movie_df[,1] <- as.character(movie_df[,1])
+    movie_df[,2] <- as.character(movie_df[,2])
+    movie_df[,3] <- as.character(movie_df[,3])
+    sub <- grep("SNL", movie_df[,3])
+    movie_df <- movie_df[-sub, ]
+    sub <- grep("Saturday Night Live", movie_df[,3])
+    movie_df <- movie_df[-sub, ]
+    sub <- grep("Daily Show", movie_df[,3])
+    movie_df <- movie_df[-sub, ]
+    
+    movie_score_df <- data.frame(NULL)
+    for (i in 1:nrow(movie_df)){
+        val <- movie_df[i, 2] == "TV"
+        if(val){show_type = "t"} else{show_type = "m"}
+        rt <- "http://www.rottentomatoes.com/search/?search="
+        search <- gsub(" ", "+", movie_df[i, 2])
+        search_sub <- gsub(":", "", search)
+        search_sub <- gsub("Â", "", search)
+        search_sub <- gsub("'", "", search)
+        movie_searchURL <- paste(rt, search, sep ="")
+        result_search <- getURL(movie_searchURL)
+        if (result_search  == "") {
+            rt <- "http://www.rottentomatoes.com/"
+            movie_title <- gsub(" ", "_", movie_df[i, 2])
+            movie_nameURL <- paste(rt, show_type, "/", movie_title, sep ="")
+            movie_page <- getURL(movie_nameURL)
+            ## rework for /t/ if tv show if(movie_page == "") {next}
+            movie_page_html <- htmlTreeParse(movie_page)
+            movie_root <- xmlRoot(movie_page_html)
+            movie_list <- xmlToList(movie_root)
+            movie_head <- movie_list$head
+            movie_body <- movie_list$body
+            if (length(movie_body[[14]]) == 0) {next}
+            score <- movie_head[[42]][2] 
+            score_num <- as.numeric(substr(score, 1,2))
+           if (length(grep("liked it" , score)) == 0) {next}
+            
+            movie_score_df[i, 1] <- movie_df[i, 1]
+            movie_score_df[i, 2] <- movie_df[i, 2]
+            movie_score_df[i, 3] <- score_num
+            movie_score_df[i, 4] <- movie_nameURL
+            names(movie_score_df) <- c("Actor", "Movie", "Audience_Score", "URL")
+        }else {
+            result <- htmlTreeParse(result_search)
+            result_root <- xmlRoot(result)
+            result_list <- xmlToList(result_root)
+            result_body <- result_list$body
+            ## this skips any 
+            if (length(result_body[[14]][[3]][[1]]) == 13) {next}
+            ## this next line pulls in the location of the first movie 
+            ##to match the search criteria
+            title <- result_body[[14]][[3]][[1]][[3]][[3]][[2]][[6]]
+            
+            rt <- "http://www.rottentomatoes.com"
+            movie_nameURL <- paste(rt, title[4], sep ="")
+            movie_page <- getURL(movie_nameURL)
+            movie_page_html <- htmlTreeParse(movie_page)
+            movie_root <- xmlRoot(movie_page_html)
+            movie_list <- xmlToList(movie_root)
+            movie_head <- movie_list$head
+            score <- movie_head[[42]][2] 
+            score_num <- as.numeric(substr(score, 1,2))
+            if (is.na(score_num)) {next}
+            
+            movie_score_df[i, 1] <- movie_df[i, 1]
+            movie_score_df[i, 2] <- movie_df[i, 2]
+            movie_score_df[i, 3] <- score_num
+            movie_score_df[i, 4] <- movie_nameURL
+            names(movie_score_df) <- c("Actor", "Movie", "Audience_Score", "URL")
+        }
+    }
     save_loc <- paste(dir, save_name, ".csv", sep = "")
     write.csv(movie_score_df, save_loc, row.names = F)
     movie_score_df <<- movie_score_df
-
-    
-}
 }
--- movie_list.R
+++ movie_list.R
@@ -2,7 +2,6 @@ movie_list <- function(dir = "./", file_name = "Movie_List", load_vector = "Acto
 
 library(RCurl)
 library(jsonlite)
-library(xml2)
 library(XML)
 
 ## second pull in movies and tv shows that the actor has appeared in from IMDB
@@ -25,6 +24,7 @@ if (exists(load_vector)){
 movie_df <- data.frame(NULL)
 
 Actor_df <- Actor_df[!duplicated(Actor_df$Actor),]
+Actor_df$Actor <- as.character(Actor_df$Actor)
 count <- 0  
 for(a in 1: nrow(Actor_df)) {
     split_name <- strsplit(Actor_df$Actor[a], " ")
@@ -38,23 +38,27 @@ for(a in 1: nrow(Actor_df)) {
     
     result_begURL <- "http://www.imdb.com/name/"
     result_endURL <- "/?ref_=fn_al_nm_1"
-    
-    actor_URL <- paste (result_begURL, id, result_endURL, sep = "")
+    actor_URL <- paste(result_begURL, id, result_endURL, sep = "")
     actor_page <- readLines(actor_URL)
-    r <- grepl("</a></b>", actor_page)
-    if (length(r[r == "TRUE"]) == 0){next}
+    r <- grep("</a></b>", actor_page)
+    if (length(r) == 0){next}
     
     sub <- actor_page[r]
     movie_list <- substr(sub, 2, nchar(sub)-8)
+    show_cat <- actor_page[(r+1)]
     for (z in 1:length(movie_list)) {
         count <- count +1
         movie_df[count, 1] <- Actor_df$Actor[a]
-        movie_df[count, 2] <- movie_list[z]
+        if (show_cat[z] == "(TV Series)") {show_category <- "TV"} else {show_category <- "Movie"}
+        movie_df[count, 2] <- show_category
+        movie_df[count, 3] <- movie_list[z]
+        
     }
 }
 
 movie_df[,1] <- as.factor(movie_df[,1])
-names(movie_df) <- c("Actor", "Movie")
+movie_df[,2] <- as.factor(movie_df[,2])
+names(movie_df) <- c("Actor",  "Category", "Name")
 save_loc <- paste(dir, file_name, ".csv", sep="")
 write.csv(movie_df, save_loc, row.names = F) 
 movie_df <<- movie_df
